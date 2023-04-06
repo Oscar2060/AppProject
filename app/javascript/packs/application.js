6fc6bafa01d902f5965b8d582761df6da37f5b6a
Oscar Tinoco <otinoco@uccs.edu>
2023-04-06 02:32:33 +0000

    finished setting up bootstrap

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "bootstrap"
+// This is your stylesheet file where you will write all the styling code.
+import "../stylesheets/application" 
 
 Rails.start()
 Turbolinks.start()
